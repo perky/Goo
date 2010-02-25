c760a8adea09071aefac08ab677a95febe26d0aa
Luke Perkin <luke.perkin@gmail.com>
2010-02-25 20:14:46 +0000

    First commit
    Signed-off-by: Luke Perkin <luke.perkin@gmail.com>

---
+++ main.lua
@@ -0,0 +1,42 @@
+-- Filename: main.lua
+-- Author: Luke Perkin
+-- Date: 2010-02-25
+
+-- Initialization
+
+function love.load()
+
+end
+
+-- Logic
+function love.update(dt)
+
+end
+
+-- Scene Drawing
+function love.draw()
+
+end
+
+-- Input
+function love.keypressed( key, unicode )
+
+end
+
+function love.keyreleased( key, unicode )
+
+end
+
+function love.mousepressed( x, y, button )
+
+end
+
+function love.mousereleased( x, y, button )
+
+end
+
+function joystickpressed( joystick, button )
+end
+
+function joystickreleased( joystick, button )
+end
