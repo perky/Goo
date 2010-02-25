83c825db1d805dffbbd72f6c5830821e42217529
Luke Perkin <luke.perkin@gmail.com>
2010-02-25 22:32:52 +0000

    'goo close button' stable.

--- Goolib/goo.lua
+++ Goolib/goo.lua
@@ -6,29 +6,55 @@
 require 'MiddleClass'
 require 'MindState'
 
-
 goo = {}
 
 goo.object = class('goo object')
 goo.objects = {}
 function goo.object:initialize()
 	table.insert(goo.objects, self)
+	--table.insert(super.children, self)
 	self.x = 0
 	self.y = 0
 	self.h = 0
 	self.w = 0
+	self.bounds = {x1=0,y1=0,x2=0,y2=0}
+	self.color  = {255,255,255,255}
+	self.children = {}
 	self.visible = true
+	self.hoverState = false
+end
+function goo.object:update(dt)
+	if self:isMouseHover() then
+		if not self.hoverState then self:enterHover() end
+		self.hoverState = true
+	else
+		if self.hoverState then self:exitHover() end
+		self.hoverState = false
+	end
+end
+function goo.object:mousepressed()
+end
+function goo.object:mousereleased()
+end
+function goo.object:keypressed()
+end
+function goo.object:keyreleased()
 end
 function goo.object:setPos( x, y )
 	self.x = x or 0
 	self.y = y or 0
+	self:updateBounds()
 end
 function goo.object:setSize( w, h )
 	self.w = w or 0
 	self.h = h or 0
+	self:updateBounds()
 end
 function goo.object:setVisible( bool )
 	self.visible = bool
+end
+function goo.object:setColor(r,g,b,a)
+	self.color = {r or self.color[1], g or self.color[2], b or self.color[3], a or self.color[4]}
 end
 function goo.object:getRelativePos( x, y )
 	local _x, _y
@@ -39,6 +65,27 @@ function goo.object:getRelativePos( x, y )
 		_x, _y = 0, 0
 	end
 	return _x+x, _y+y
+end
+function goo.object:isMouseHover()
+	if not self.bounds then return false end
+	local x, y = love.mouse.getPosition()
+	local x1, y1, x2, y2 = self.bounds.x1, self.bounds.y1, self.bounds.x2, self.bounds.y2
+	if x > x1 and x < x2 and y > y1 and y < y2 then
+		return true
+	else
+		return false
+	end
+end
+function goo.object:enterHover()
+end
+function goo.object:exitHover()
+end
+function goo.object:updateBounds()
+	local x, y = self:getRelativePos()
+	self.bounds.x1 = x
+	self.bounds.y1 = y
+	self.bounds.x2 = x + self.w
+	self.bounds.y2 = y + self.h
 end
 function goo.object:destroy()
 	for k,v in pairs( goo.objects ) do
@@ -63,29 +110,42 @@ function goo.panel:draw()
 	love.graphics.print( self.title, self.x + 10, self.y + 15 )
 	love.graphics.line( self.x, self.y + 20, self.x + self.w, self.y + 20)
 end
+function goo.panel:mousepressed(x,y,button)
+	print('panel clicked')
+end
 function goo.panel:setTitle( title )
 	self.title = title
 end
 function goo.panel:setPos( x, y )
 	super.setPos(self, x, y)
 	self:setClosePos()
+	self:updateBounds()
 end
 function goo.panel:setSize( w, h )
 	super.setSize(self, w, h)
 	self:setClosePos()
+	self:updateBounds()
 end
 function goo.panel:setClosePos()
 	self.close:setPos( self.w - 15, 15 )
+end
+function goo.panel:destroy()
+	for k,v in pairs(self.children) do
+		v:destroy()
+	end
+	super.destroy(self)
 end
 -- STATIC TEXT
 goo.text = class('goo static text', goo.object)
 function goo.text:initialize( parent )
 	super.initialize(self)
+	table.insert(parent.children, self)
 	self.parent = parent
 	self.text = "no text"
 end
 function goo.text:draw()
 	local x, y = self:getRelativePos()
+	love.graphics.setColor( unpack(self.color) )
 	love.graphics.print( self.text, x, y )
 end
 function goo.text:setText( text )
@@ -95,16 +155,30 @@ end
 goo.close = class('goo close button', goo.object)
 function goo.close:initialize( parent )
 	super.initialize(self)
+	table.insert(parent.children, self)
 	self.parent = parent
 end
-function goo.close:update(dt)
+function goo.close:enterHover()
+	self.color = {255,0,0,255}
+end
+function goo.close:exitHover()
+	self.color = {255,255,255,255}
 end
 function goo.close:draw()
 	local x, y = self:getRelativePos()
-	love.graphics.setColor(255,255,255,255)
+	love.graphics.setColor( unpack(self.color) )
 	love.graphics.print('x', x, y)
 end
 function goo.close:mousereleased(x,y,button)
+	print("click")
+	self.parent:destroy()
+end
+function goo.close:updateBounds()
+	local x, y = self:getRelativePos()
+	self.bounds.x1 = x
+	self.bounds.y1 = y-10
+	self.bounds.x2 = x+10
+	self.bounds.y2 = y
 end
 
 testPanel = goo.panel:new()
@@ -150,13 +224,30 @@ function goo.keyreleased( key, unicode )
 end
 
 function goo.mousepressed( x, y, button )
-	for k,v in ipairs( goo.objects ) do
-		if v.visible then v:mousepressed(x, y, button) end
+	for k,v in ipairs_back( goo.objects ) do
+		if v.visible and v:isMouseHover() then 
+			v:mousepressed(x, y, button)
+			break
+		end
 	end
 end
 
 function goo.mousereleased( x, y, button )
-	for k,v in ipairs( goo.objects ) do
-		if v.visible then v:mousepressed(x, y, button) end
+	for k,v in ipairs_back( goo.objects ) do
+		if v.visible and v:isMouseHover() then 
+			v:mousereleased(x, y, button)
+			break
+		end
+	end
+end
+
+function ipairs_back(a)
+	return ipairs_back_iter, a, #a
+end
+function ipairs_back_iter(a,i)
+	i = i - 1
+	local v = a[i]
+	if v then
+		return i, v
 	end
 end
